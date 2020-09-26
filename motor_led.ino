728f12ab91e22edc0d0fb8b4df520bcf6f777b6e
ndev <napodevesa@gmail.com>
2020-09-26 10:33:55 +0200

    init 2

--- motor_led.ino
+++ motor_led.ino
@@ -1,42 +1,59 @@
 
+#include <pt.h>
 #include <Stepper.h>
-const int ledPIN = 7;
-const int stepsPerRevolution = 32*64;  // change this to fit the number of steps per revolution
 
-// initialize the stepper library on pins 8 through 11:
+
+struct pt hilo1;
+
+const int stepsPerRevolution = 32*64;  
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
 
-void setup() {
-  pinMode(ledPIN , OUTPUT);  //definir pin como salida
+void setup() 
+{
+  
+  PT_INIT(&hilo1);  
   // set the speed at 15 rpm:
   myStepper.setSpeed(15);
   // initialize the serial port:
   Serial.begin(9600);
+  
 }
 
 
 
-void loop() {
+void loop() 
+{
+  parpadeo (&hilo1);
+  
   // step one revolution  in one direction:
   Serial.println("clockwise");
   myStepper.step(stepsPerRevolution);
-  
-  delay(500);
-
-  digitalWrite(ledPIN , HIGH);   // poner el Pin en HIGH
-  delay(500);                   // esperar un segundo
-  digitalWrite(ledPIN , LOW);    // poner el Pin en LOW
-  delay(500);
-
-  digitalWrite(ledPIN , HIGH);   // poner el Pin en HIGH
-  delay(500);                   // esperar un segundo
-  digitalWrite(ledPIN , LOW);    // poner el Pin en LOW
   delay(500);
 
   // step one revolution in the other direction:
   Serial.println("counterclockwise");
   myStepper.step(-stepsPerRevolution);
   delay(500);
-
    
+}
+
+void parpadeo(struct pt *pt) {
+  PT_BEGIN(pt);
+  // void setup() {
+  static long t = 0;
+  pinMode(7, OUTPUT);
+  // }
+  
+  do {
+  // void loop() {
+    digitalWrite(10, HIGH);
+    t = millis();
+    PT_WAIT_WHILE(pt, (millis()-t)<1000);
+    
+    digitalWrite(10, LOW);
+    t = millis();
+    PT_WAIT_UNTIL(pt, (millis()-t)>=1000);
+  // }
+  } while(true);
+  PT_END(pt);
 }
