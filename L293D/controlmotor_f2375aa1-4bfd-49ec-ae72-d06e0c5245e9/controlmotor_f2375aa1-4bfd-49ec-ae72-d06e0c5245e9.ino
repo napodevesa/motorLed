065910645c629dc4261a9a847ac786298eb830c1
ndev <napodevesa@gmail.com>
2020-11-21 00:01:57 +0100

    servo

---
+++ L293D/controlmotor_f2375aa1-4bfd-49ec-ae72-d06e0c5245e9/controlmotor_f2375aa1-4bfd-49ec-ae72-d06e0c5245e9.ino
@@ -0,0 +1,16 @@
+const int motor1A = 2;
+const int motor1B = 4;
+int velocidad = 0;
+const int pwm1 = 2;
+void setup() {
+  pinMode(motor1A, OUTPUT);
+  pinMode(motor1B, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(motor1A, HIGH);
+  digitalWrite(motor1B, LOW);
+  velocidad = 2;
+  analogWrite(pwm1, velocidad);
+}
---
+++ servo_656795dc-10f4-474b-abc4-978510c74670/servo_656795dc-10f4-474b-abc4-978510c74670.ino
@@ -0,0 +1,25 @@
+#include<Servo.h>
+
+
+int servo1 = 9;
+int potPin = A0;
+int potValue = 0;
+
+Servo servo;
+int angulo = 0;
+
+
+void setup() {
+  servo.attach(servo1);
+
+}
+
+void loop() {
+
+    potValue =analogRead(potPin);
+    angulo = map(potValue, 0, 700, 0, 180);
+    servo.write(angulo);
+    delay(30);
+   
+
+}
